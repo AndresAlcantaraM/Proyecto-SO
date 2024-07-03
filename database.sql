6e326cae352d70965ef32943b90aab3580ef663b
AndresAlcantaraM <163611700+AndresAlcantaraM@users.noreply.github.com>
2024-07-03 10:20:04 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,18 @@
 CREATE TABLE ejecuciones (
     id SERIAL PRIMARY KEY,
-    comandos JSONB NOT NULL,
     algoritmo VARCHAR(50) NOT NULL,
-    tiempos JSONB NOT NULL
+    avg_response_time FLOAT,
+    avg_turnaround_time FLOAT
 );
+
+CREATE TABLE comandos(
+    id SERIAL PRIMARY KEY,
+    ejecucion_id INT REFERENCES ejecuciones(id),
+    comando VARCHAR(255) NOT NULL,
+    tiempo_inicio INT NOT NULL,
+    tiempo_estimado INT NOT NULL,
+    imagen VARCHAR(255) NOT NULL,
+    tiempo_final INT,
+    response_time INT,
+    turnaround_time INT
+)
